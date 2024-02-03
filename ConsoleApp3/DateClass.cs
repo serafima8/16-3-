cda9c79480b0c5f60d9d3b5f68df37362ebe92c5
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-02-03 21:51:42 +0200

    Add files via upload

---
+++ ConsoleApp3/DateClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class DateClass
+    {
+        /// <summary>
+        /// Создание поля DateTime data
+        /// </summary>
+        DateTime data;
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public DateClass() { }
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <param name="day"></param>
+        public DateClass(int year, int month, int day)
+        {
+            data = new DateTime(year, month, day);
+            year = year;
+            month = month;
+            day = day;
+        }
+        /// <summary>
+        /// Свойства поля DateTime data
+        /// </summary>
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; } 
+        }
+        /// <summary>
+        ///  Метод, позволяющий вычислить дату предыдущего дня
+        /// </summary>
+        /// <returns></returns>
+        public DateTime Previous()
+        {
+            return data.AddDays(-1);
+        }
+        /// <summary>
+        /// Метод, позволяющий вычислить дату последующего дня
+        /// </summary>
+        /// <returns></returns>
+        public DateTime Next()
+        {
+            return data.AddDays(1);
+        }
+        /// <summary>
+        /// Метод, позволяющий вычислить кол-во дней до конца месяца 
+        /// </summary>
+        public void Kolvo()
+        {
+            DateTime DaysInMonth = new DateTime();
+            Console.WriteLine($"Количество дней до конца месяца: {DateTime.DaysInMonth(data.Year, data.Month) - data.Day}");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+
+            Console.Write("Year ");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("Month ");
+            int month = int.Parse(Console.ReadLine());
+            Console.Write("Day ");
+            int day = int.Parse(Console.ReadLine());
+            DateClass date = new DateClass(year, month, day);
+            DateTime date1 = new DateTime(year, month, day);
+            Console.WriteLine(date1.ToShortDateString());
+            Console.WriteLine($"Предыдущий день: {date.Previous().ToShortDateString()}");
+            Console.WriteLine($"Последующий день: {date.Next().ToShortDateString()}");
+            date.Kolvo();
+
+            //2
+
+            Console.Write("Введите первую дату: ");
+            DateTime date2 = DateTime.Parse(Console.ReadLine());
+            Console.Write("Введите вторую дату: ");
+            DateTime date3 = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine($"Количество времени между датами: {date3.Subtract(date2)}");
+
+            //3
+
+            Console.Write("Введите время 1 процедуры: ");
+            DateTime time1 = DateTime.Parse(Console.ReadLine());
+            Console.Write("Введите время 2 процедуры: ");
+            DateTime time2 = DateTime.Parse(Console.ReadLine());
+            Console.Write("Введите общее количество процедур: ");
+            int kolvo = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < kolvo; i++)
+            {
+                DateTime a = time1.AddMinutes(i * (time2 - time1).TotalMinutes);
+                Console.WriteLine($"Расписание процедуры {i + 1}: {a.ToShortTimeString()}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
